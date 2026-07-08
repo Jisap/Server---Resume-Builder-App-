7032b57f3e3ddc22f8edb241d0185826e14628cc
Jorge Matos <jisapjisap@gmail.com>
2026-07-08 18:16:29 +0200

    feat: implement CRUD controller functions for resume management

--- controllers/resumeController.js
+++ controllers/resumeController.js
@@ -4,12 +4,68 @@ import Resume from "../models/Resume";
 // Controller for creating a new resume
 // POST: /api/resumes/create
 export const createResume = async (req, res) => {
-    try {
-        const userId = req.userId;
-        const { title } = req.body;
-        const newResume = await Resume.create({ userId, title });
-        res.status(201).json({ message: "Resume created successfully", resume: newResume });
-    } catch (error) {
-        res.status(400).json({ message: error.message });
+  try {
+    const userId = req.userId;
+    const { title } = req.body;
+    const newResume = await Resume.create({ userId, title });
+    res.status(201).json({ message: "Resume created successfully", resume: newResume });
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+}
+
+// Controller for deleting a resume
+// DELETE: /api/resumes/delete
+export const deleteResume = async (req, res) => {
+  try {
+    const userId = req.userId;
+    const { resumeId } = req.params;
+
+    await Resume.findOneAndDelete({ userId, _id: resumeId })
+
+    res.status(201).json({ message: "Resume deleted successfully" });
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+}
+
+// get user resume by id
+// GET: /api/resumes/get
+export const getResumeById = async (req, res) => {
+  try {
+    const userId = req.userId;
+    const { resumeId } = req.params;
+
+    const resume = await Resume.findOne({ userId, _id: resumeId })
+    if (!resume) {
+      return res.status(404).json({ message: "Resume not found" });
     }
+
+    resume.__v = undefined;
+    resume.createdAt = undefined;
+    resume.updatedAt = undefined;
+
+    return res.status(200).json({ resume });
+
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+}
+
+// get resume by id public
+// GET: /api/resumes/public
+export const getPublicResumeById = async (req, res) => {
+  try {
+    const { resumeId } = req.params;
+
+    const resume = await Resume.findOne({ _id: resumeId, public: true })
+    if (!resume) {
+      return res.status(404).json({ message: "Resume not found" });
+    }
+
+    return res.status(200).json({ resume });
+
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
 }
